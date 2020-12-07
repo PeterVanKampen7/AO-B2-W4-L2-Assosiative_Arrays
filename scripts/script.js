3012fdbbf96448593f447c6adfe1f90072a4cf3d
Peter <99065921@mydavinci.nl>
2020-12-07 12:01:35 +0100

    Done for 2.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,26 @@
 window.onload = function(){	//Wacht tot de hele pagina is geladen voordat het JS script start
 var run = true;
 
-var fris = 0;	//Een set variabelen voor het bijhouden hoeveel er besteld is
-var bier = 0;
-var wijn = 0;
-var bitterballen = 0;
+var orders = {
+	fris : 0,
+	bier : 0,
+	wijn : 0,
+	bitterballen : 0
+}
 
-var frisPrijs;	//Een set variabelen waar de totaal prijs per product in zal komen
-var bierPrijs;
-var wijnPrijs;
-var bitterbalPrijs;
+const PRIJZEN = {
+	fris : 2.5,
+	bier : 1.75,
+	wijn : 4,
+	bitterballen : 0.25
+}
+
+var eindPrijzen = {
+	fris : 0,
+	bier : 0,
+	wijn : 0,
+	bitterballen : 0
+}
 
 while(run)
 {
@@ -17,6 +28,7 @@ while(run)
 	if(input == "stop")
 	{
 		run = false;
+		console.log(orders['fris']);
 		rekening();
 	}
 	else if(input == "fris" || input == "bier" || input == "wijn")
@@ -40,15 +52,15 @@ function addDrink(order)	//Een functie die het toevoegen van drankjes afhandeld.
 
 	if(order == "fris")
 	{
-		fris+=amount;
+		orders.fris+=amount;
 	}
 	else if(order == "bier")
 	{
-		bier+=amount;
+		orders.bier+=amount;
 	}
 	else
 	{
-		wijn+=amount;
+		orders.wijn+=amount;
 	}
 }
 
@@ -58,110 +70,108 @@ function addSnacks()	//Een functie die het toevoegen van bitterbalen afhandeld. 
 	if(amount == 8 || amount == 16)
 	{
 		var schalen = Number(prompt("Hoeveel schalen van "+amount+" bitterballen wilt u bestellen?"));
-		bitterballen += schalen * amount;
+		orders.bitterballen += schalen * amount;
 	}
 	else
 	{
 		alert("U kunt alleen een keuze maken tussen 8 en 16. De snacks zijn niet toegevoegd aan de bestelling.");
 		addSnacks();
 	}
-
-	console.log("bitterballen: "+bitterballen);
 }
 
 function rekening()	//Een functie die de rekening maakt en op het scherm zet.
 {
-	frisPrijs = prijzen("fris");	//Doormiddel van de prijzen functie word de totaal prijs per product bepaalt.
-	bierPrijs = prijzen("bier");
-	wijnPrijs = prijzen("wijn");
-	bitterbalPrijs = prijzen("bitterballen"); 
+	eindPrijzen.fris = prijzen("fris");	//Doormiddel van de prijzen functie word de totaal prijs per product bepaalt.
+	eindPrijzen.bier = prijzen("bier");
+	eindPrijzen.wijn = prijzen("wijn");
+	eindPrijzen.bitterbal = prijzen("bitterballen"); 
 
 	var writeLine = 1;	//Een variabele om bij te houden op welke regel van het scherm er geprint moet worden
 
-	if(fris != 0)	//Als er fris besteld is zet dit op de rekening
+	if(orders.fris != 0)	//Als er fris besteld is zet dit op de rekening
 	{
 		document.getElementById("item1").innerHTML = "Fris";
-		document.getElementById("qty1").innerHTML = fris;
-		document.getElementById("prijs1").innerHTML = "\u20AC"+frisPrijs;
+		document.getElementById("qty1").innerHTML = orders.fris;
+		document.getElementById("prijs1").innerHTML = "\u20AC"+eindPrijzen.fris;
 		writeLine++;
 	}
 
-	if(bier != 0)	//Als er bier besteld is zet dit op de rekening op een nog lege lijn
+	if(orders.bier != 0)	//Als er bier besteld is zet dit op de rekening op een nog lege lijn
 	{
 		if(writeLine == 1)
 		{
 			document.getElementById("item1").innerHTML = "Bier";
-			document.getElementById("qty1").innerHTML = bier;
-			document.getElementById("prijs1").innerHTML = "\u20AC"+bierPrijs;
+			document.getElementById("qty1").innerHTML = orders.bier;
+			document.getElementById("prijs1").innerHTML = "\u20AC"+eindPrijzen.bier;
 			writeLine++;
 		}
 		else
 		{
 			document.getElementById("item2").innerHTML = "Bier";
-			document.getElementById("qty2").innerHTML = bier;
-			document.getElementById("prijs2").innerHTML = "\u20AC"+bierPrijs;
+			document.getElementById("qty2").innerHTML = orders.bier;
+			document.getElementById("prijs2").innerHTML = "\u20AC"+eindPrijzen.bier;
 			writeLine++;
 		}
 	}
 
-	if(wijn != 0)	//Als er wijn besteld is zet dit op de rekening op een nog lege lijn
+	if(orders.wijn != 0)	//Als er wijn besteld is zet dit op de rekening op een nog lege lijn
 	{
 		if(writeLine == 1)
 		{
 			document.getElementById("item1").innerHTML = "Wijn";
-			document.getElementById("qty1").innerHTML = wijn;
-			document.getElementById("prijs1").innerHTML = "\u20AC"+wijnPrijs;
+			document.getElementById("qty1").innerHTML = orders.wijn;
+			document.getElementById("prijs1").innerHTML = "\u20AC"+eindPrijzen.wijn;
 			writeLine++;
 		}
 		else if(writeLine == 2)
 		{
 			document.getElementById("item2").innerHTML = "Wijn";
-			document.getElementById("qty2").innerHTML = wijn;
-			document.getElementById("prijs2").innerHTML = "\u20AC"+wijnPrijs;
+			document.getElementById("qty2").innerHTML = orders.wijn;
+			document.getElementById("prijs2").innerHTML = "\u20AC"+eindPrijzen.wijn;
 			writeLine++;
 		}
 		else
 		{
 			document.getElementById("item3").innerHTML = "Wijn";
-			document.getElementById("qty3").innerHTML = wijn;
-			document.getElementById("prijs3").innerHTML = "\u20AC"+wijnPrijs;
+			document.getElementById("qty3").innerHTML = orders.wijn;
+			document.getElementById("prijs3").innerHTML = "\u20AC"+eindPrijzen.wijn;
 			writeLine++;
 		}
 	}
 
-	if(bitterballen != 0)	//Als er bitterballen besteld zijn zet dit op de rekening op een nog lege lijn
+	if(orders.bitterballen != 0)	//Als er bitterballen besteld zijn zet dit op de rekening op een nog lege lijn
 	{
 		if(writeLine == 1)
 		{
 			document.getElementById("item1").innerHTML = "Bitterballen";
-			document.getElementById("qty1").innerHTML = bitterballen;
-			document.getElementById("prijs1").innerHTML = "\u20AC"+bitterbalPrijs;
+			document.getElementById("qty1").innerHTML = orders.bitterballen;
+			document.getElementById("prijs1").innerHTML = "\u20AC"+eindPrijzen.bitterbal;
 			writeLine++;
 		}
 		else if(writeLine == 2)
 		{
 			document.getElementById("item2").innerHTML = "Bitterballen";
-			document.getElementById("qty2").innerHTML = bitterballen;
-			document.getElementById("prijs2").innerHTML = "\u20AC"+bitterbalPrijs;
+			document.getElementById("qty2").innerHTML = orders.bitterballen;
+			document.getElementById("prijs2").innerHTML = "\u20AC"+eindPrijzen.bitterbal;
 			writeLine++;
 		}
 		else if(writeLine == 3)
 		{
 			document.getElementById("item3").innerHTML = "Bitterballen";
-			document.getElementById("qty3").innerHTML = bitterballen;
-			document.getElementById("prijs3").innerHTML = "\u20AC"+bitterbalPrijs;
+			document.getElementById("qty3").innerHTML = orders.bitterballen;
+			document.getElementById("prijs3").innerHTML = "\u20AC"+eindPrijzen.bitterbal;
 			writeLine++;
 		}
 		else
 		{
 			document.getElementById("item4").innerHTML = "Bitterballen";
-			document.getElementById("qty4").innerHTML = bitterballen;
-			document.getElementById("prijs4").innerHTML = "\u20AC"+bitterbalPrijs;
+			document.getElementById("qty4").innerHTML = orders.bitterballen;
+			document.getElementById("prijs4").innerHTML = "\u20AC"+eindPrijzen.bitterbal;
 			writeLine++;
 		}
 	}
 
-	var totalQty = fris + bier + wijn + bitterballen;	//De totale hoeveelheid producten
+	var totalQty = orders.fris + orders.bier + orders.wijn + orders.bitterballen;	//De totale hoeveelheid producten
 	var totalPrijs = prijzen("totaal");		//De totaalprijs van de hele bestelling word bepaald doormiddel van een functie
 											//De totaalprijs word op de rekening gezet op een nog lege lijn
 	if(writeLine == 2)
@@ -194,23 +204,23 @@ function prijzen(type)	//Een functie die het totaal en subtotaal van de bestelli
 {
 	if(type == "fris")
 	{
-		return fris * 2.5;
+		return orders.fris * PRIJZEN.fris;
 	}
 	else if(type == "bier")
 	{
-		return bier * 1.75;
+		return orders.bier * PRIJZEN.bier;
 	}
 	else if(type == "wijn")
 	{
-		return wijn * 4;
+		return orders.wijn * PRIJZEN.wijn;
 	}
 	else if(type == "bitterballen")
 	{
-		return bitterballen * 0.25;
+		return orders.bitterballen * PRIJZEN.bitterballen;
 	}
 	else if(type == "totaal")
 	{
-		return frisPrijs + bierPrijs + wijnPrijs + bitterbalPrijs;
+		return eindPrijzen.fris + eindPrijzen.bier + eindPrijzen.wijn + eindPrijzen.bitterbal;
 	}
 }
 }
